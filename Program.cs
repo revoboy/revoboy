182594352ba83b8fe24bf51d4e364b9ec11e6462
revoboy <88243401+revoboy@users.noreply.github.com>
2021-08-01 19:14:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите свое имя:");
+            string name = Console.ReadLine();
+            name = name.Trim();
+
+            Console.WriteLine("введите свой возраст:");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("введите свой адрес:");
+            string adress = Console.ReadLine();
+            adress = adress.Trim();
+            adress = adress.Replace(".", ",");
+
+            Console.WriteLine("введите свой рост:");
+            string height = Console.ReadLine();
+            height = height.Replace(".", ",");
+            Convert.ToSingle(height);
+
+            Console.WriteLine("введите свой вес:");
+            string weight = Console.ReadLine();
+            weight = weight.Replace(".", ",");
+            Convert.ToSingle(weight);
+
+            Console.WriteLine("введите свой номер телефона: (без +380)");
+            string phone = Console.ReadLine();
+           
+            Console.WriteLine("\n-------------------- \nВаши данные: ");
+            Console.WriteLine("Имя: " + name + "\nВозраст: " + age + " года" + "\nАдрес: " + adress  + "\nРост: " + height + " см" + "\nВес: " + weight + " кг" + "\nНомер телефона: +380" + phone);
+           
+
+
+
+
+
+
+
+        }  
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название продукта:");
+            string name = Console.ReadLine();
+            name = name.Trim();
+            name = name.ToUpper();
+
+            Console.WriteLine("Введите количество единиц товара:");
+            string amount = Console.ReadLine();
+            amount = amount.Replace(".", ",");
+            Convert.ToDecimal(amount);
+
+            Console.WriteLine("Введите единицу измерения: \n(шт., кг, тонны, литры и тд)");
+            string units = Console.ReadLine();
+            units = units.ToLower();
+
+            Console.WriteLine("Введите цену за единицу товара в долларах:");
+            string price = Console.ReadLine();
+            price = price.Replace(".", ".");
+            Convert.ToDecimal(price);
+
+            Console.Clear();
+
+            Console.WriteLine("\n-------------------- \nВаши данные: ");
+            Console.WriteLine("\nНазвание продкта: " + name + "\nКоличество: " + amount + units + "\nЦена: " + price + "$"); 
+
+        }
+    }
+}
